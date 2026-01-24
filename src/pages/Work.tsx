5e7e7bdb8c34a2e6a769e46016321ec10a156725
Zeal Shah <75566085+zealshah29@users.noreply.github.com>
2026-01-24 16:26:21 +0530

    Change resume link in Work component
    Updated the link in onRESUMETextClick to use a new URL.

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -26,7 +26,7 @@ const Work: FunctionComponent = () => {
 
   const onRESUMETextClick = useCallback(() => {
     window.open(
-      "https://drive.google.com/file/d/1QKfTg4IxINo5708_-KW1kGMgXLrsZtfZ/view?usp=sharing"
+      "https://drive.google.com/file/d/10mhFlizrg0A4noi5ZEh7_EhJ4scDun24/view?usp=drive_link"
     );
   }, []);
 
